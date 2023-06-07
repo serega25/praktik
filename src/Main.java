b2e3626444d90bda028c6a9b6a4d2fdbcac05641
Сергей Тимошко <sergeytimoshko2512@gmail.com>
2023-06-07 19:46:38 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Random;
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+int num=8;
+System.out.println(num);
+System.out.println(num>>1);
+
+    }
+    }
+
+
+
